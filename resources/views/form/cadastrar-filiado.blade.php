a9db12997ba013423a95fea752c23e92ce8a455e
UsernameAutentic <elyzeumozartcardoso@gmail.com>
2022-12-08 17:17:59 -0300

    validação cpf

---
+++ resources/views/form/cadastrar-filiado.blade.php
@@ -0,0 +1,41 @@
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Cadastrar Filiado') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <x-jet-validation-errors class="mb-4" />
+            <form method="POST" action="{{ route('cadastrar-filiado-save') }}">
+            @csrf
+
+
+            <div class="mt-4">
+                <x-jet-label for="nome" value="{{ __('Nome') }}" />
+                <x-jet-input id="nome" class="block mt-1 w-full" type="text" name="nome" :value="old('nome')" required />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="descricao" value="{{ __('Descricao') }}" />
+                <x-jet-input id="descricao" class="block mt-1 w-full" type="text" name="descricao" required autocomplete="descricao" />
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="data" value="{{ __('Data') }}" />
+                <x-jet-input id="data" class="block mt-1 w-full" type="date" name="data" required autocomplete="data" />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+
+                <x-jet-button class="ml-4">
+                    {{ __('Send') }}
+                </x-jet-button>
+            </div>
+        </form>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
